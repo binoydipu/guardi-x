e6e03f924b9a6a38a6ada4d6948db96ec34fb9f0
Binoy Barman <binoydipu@gmail.com>
2024-11-19 01:12:27 +0600

    created welcome view

---
+++ lib/constants/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Primary Colors
+const midnightBlueColor = Color(0xFF003566);
+const skyBlueColor = Color(0xFF3A86FF);
+
+// Accent Colors
+const safetyOrangeColor = Color(0xFFFF6F00);
+const brightGreenColor = Color(0xFF36D399);
+
+// Neutral Colors
+const charcoalGreyColor = Color(0xFF333333);
+const lightGreyColor = Color(0xFFE5E5E5);
+const whiteColor = Color(0xFFFFFFFF);
+const blackColor = Color(0xFF000000);
+
+// Error/Warning COlors
+const crimsonRedColor = Color(0xFFFF0033);
