cd7392db1a0a99c74e6ec8fbf88280e5325ce80a
Binoy Barman <binoydipu@gmail.com>
2025-02-13 01:13:17 +0600

    bug fixed

--- lib/views/bottom_nav/sos_view.dart
+++ lib/views/bottom_nav/sos_view.dart
@@ -12,24 +12,14 @@ class SosView extends StatefulWidget {
 
 class _SosViewState extends State<SosView> {
   void _makePhoneCall(String phoneNumber) async {
-    final Uri phoneUri = Uri(
-      scheme: 'tel',
-      path: phoneNumber,
-    );
-
+    final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
     if (await canLaunchUrl(phoneUri)) {
       await launchUrl(phoneUri);
     } else {
-      // ignore: use_build_context_synchronously
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Could not launch the dialer')),
       );
     }
-    /*if (await canLaunchUrl(phoneUri)) {
-      await launchUrl(phoneUri);
-    } else {
-      throw 'Could not launch $phoneNumber';
-    }*/
   }
 
   @override
@@ -38,59 +28,55 @@ class _SosViewState extends State<SosView> {
       appBar: AppBar(
         leading: Navigator.canPop(context)
             ? IconButton(
-                icon: const Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: whiteColor,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+                icon: const Icon(Icons.arrow_back_ios_new_rounded,
+                    color: whiteColor),
+                onPressed: () => Navigator.of(context).pop(),
               )
             : null,
         title: const Text(
           'SOS Emergency',
-          style: TextStyle(color: whiteColor),
+          style: TextStyle(color: whiteColor, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         backgroundColor: midnightBlueColor,
+        elevation: 5,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 6,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 5, 16, 16),
-                  child: ElevatedButton(
-                    onPressed: () => _makePhoneCall(numbers[index]),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 175, 206, 255),
-                      elevation: 5,
-                      shadowColor: Colors.black26,
-                      foregroundColor: blackColor,
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 12,
-                        horizontal: 12,
-                      ),
-                      minimumSize: const Size.fromHeight(70),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                    ),
-                    child: Text(
-                      services[index],
-                      textAlign: TextAlign.center,
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: services.length,
+            itemBuilder: (context, index) {
+              return Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                elevation: 4,
+                shadowColor: Colors.black,
+                color: Colors.white,
+                child: ListTile(
+                  onTap: () => _makePhoneCall(numbers[index]),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                  leading: const CircleAvatar(
+                    backgroundColor: midnightBlueColor,
+                    child: Icon(Icons.local_phone, color: whiteColor),
+                  ),
+                  title: Text(
+                    services[index],
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
                     ),
                   ),
-                );
-              },
-            ),
+                  trailing: const Icon(Icons.arrow_forward_ios_rounded,
+                      color: Colors.black54),
+                ),
+              );
+            },
           ),
-        ],
+        ),
       ),
     );
   }
